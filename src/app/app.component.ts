71e394732b6b7479868ab55e22dfbff0ec77b779
Joban1992 <singh.jobanpreet@tcs.com>
2018-06-07 20:25:48 +0530

    Update app.component.ts
    Code indentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,33 @@
-import { Component , OnInit} from '@angular/core';
-import { UtilService } from './sharedModules/services/Util.service';
+import {
+ Component,
+ OnInit
+}
+from '@angular/core';
+import {
+ UtilService
+}
+from './sharedModules/services/Util.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'Contacts App';
-  showAlert = false;
-  alertType = '';
-  alertText = '';
-  constructor(private utilService: UtilService){
+export class AppComponent implements OnInit {
+ title = 'Contacts App';
+ showAlert = false;
+ alertType = '';
+ alertText = '';
+ constructor(private utilService: UtilService) {
 
-  }
+ }
 
-    ngOnInit(){
-    console.log('aaa', this.utilService)
-    this.utilService.alertState.subscribe((data)=>{
-       this.alertType = data.type || '';
-       this.alertText = data.message || '';
-       this.showAlert = (data.showAlert==false)? false : true;
-    });
-    }
+ ngOnInit() {
+  console.log('aaa', this.utilService)
+  this.utilService.alertState.subscribe((data) => {
+   this.alertType = data.type || '';
+   this.alertText = data.message || '';
+   this.showAlert = (data.showAlert == false) ? false : true;
+  });
+ }
 }
